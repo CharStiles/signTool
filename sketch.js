9e1f092edd4d1be87a929ab9a609d5991bd5ebbb
Char Stiles <ch4@mit.edu>
2024-02-13 21:13:33 -0500

    updates, working on gui more nect

--- sketch.js
+++ sketch.js
@@ -1,266 +1,305 @@
-// Copyright (c) 2023 ml5
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
 
 let handpose;
 let video;
 let hands = [];
-let maxLineLen = 200;
 let lineLen = 150;
 let lines = {};
-let easing = 0.945;
+let easing = 0.3;
 let d = 200;
-let indx = 0;
-let startIdx = 0;
-let gui;
-var defaulType = [47, 100, 49, 43];
-var features = ["Easing","Cam Opacity", "Trail Speed", "Trail Length", "Mirror"
-];
+let handPointInices = [0,4, 8, 12, 16, 20];
+let handPointNames = ["wrist", "thumb", "index_finger_tip", "middle_finger_tip", "ring_finger_tip", "pinky_finger_tip"];
+var features = ["Easing","Cam Opacity", "Trail Speed", "Trail Length", "Mirror"];
+let textScreen;
+
+class polylineWithVisibleData {
+  constructor(){
+    this.points = [];
+    this.visible = [];
+  }
+  addPoint(x, y, visible){
+    this.points.push(createVector(x, y, 0));
+    this.visible.push(visible);
+
+    // if we have more than 100 points, remove the first one
+    if (this.points.length > 100){
+      this.points.shift();
+      this.visible.shift();
+    }
+  }
+
+  draw(){
+      // draw as a series of line segments
+      // only draw if both points are visible
+
+
+      // copy the points array to a new array: 
+      // to add smoothing
+      let smoothedPoints = [];
+      for (let i = 0; i < this.points.length; i++){
+        smoothedPoints.push(createVector(this.points[i].x, this.points[i].y,this.points[i].z));
+        this.points[i].z -=10;
+      }
+      // smooth the points, but only smooth based on the visible points
+      for (let i = 1; i < this.points.length - 1; i++){
+        if (this.visible[i] && this.visible[i+1]){
+          smoothedPoints[i].x = (smoothedPoints[i-1].x + smoothedPoints[i].x + smoothedPoints[i+1].x) / 3;
+          smoothedPoints[i].y = (smoothedPoints[i-1].y + smoothedPoints[i].y + smoothedPoints[i+1].y) / 3;
+        }
+      }
+
+
+      // set line width 
+      strokeWeight(3);
+
+      for (let i = 0; i < this.points.length - 1; i++){
+        // calculate the alpha value based on pct through the line
+        let pct = i / this.points.length;
+        let alpha = 255 * (1 - pct);
+        stroke(255, 255,255, 255.-alpha);
+        if (this.visible[i] && this.visible[i+1]){
+          line(smoothedPoints[i].x, smoothedPoints[i].y,smoothedPoints[i].z, smoothedPoints[i+1].x, smoothedPoints[i+1].y,smoothedPoints[i+1].z);
+        }
+      }
+  }
+}
+
+
+
+//--------------------------------------------------------------------------------
+class fingerPoint {
+
+  polyline = new polylineWithVisibleData();
+  target = {};
+  bGotTarget = false;
+  
+  constructor(x, y){
+
+    
+    this.x = x;
+    this.y = y;
+    this.nFramesSinceSeen = 0;
+  }
+  setup(){ 
+    this.target = createVector(0,0);
+  }
+  increaseFramesSinceSeen(){
+    this.nFramesSinceSeen++;
+  }
+  updateTarget(x, y){
+    this.target.x = x;
+    this.target.y = y;
+    this.bGotTarget = true;
+  }
+  update(smoothing){
+
+    if (this.bGotTarget){
+      this.nFramesSinceSeen = 0;
+      this.bGotTarget = false;
+    }
+
+    if (this.nFramesSinceSeen > 20){
+      this.x = this.target.x;
+      this.y = this.target.y;
+    } else {
+      this.x = smoothing * this.x + (1-smoothing) * this.target.x;
+      this.y = smoothing * this.y + (1-smoothing) * this.target.y;
+    }
+
+    this.polyline.addPoint(this.x, this.y, this.nFramesSinceSeen < 10);
+
+  }
+
+  drawLine(){
+    this.polyline.draw();
+  }
+}
+
+//--------------------------------------------------------------------------------
+class fingerPoints {
+  constructor(){
+    this.fingers = [];
+    for (let i = 0; i < 6; i++){
+      this.fingers.push(new fingerPoint(0,0));
+    }
+  }
+
+  setup(){  
+    for (let i = 0; i < 6; i++){
+      this.fingers[i].setup();
+    }
+  }
+
+  
+  updateTarget( fingerIndex, x, y){
+    this.fingers[fingerIndex].updateTarget(x, y);
+  }
+  
+  update(smoothing){
+    for (let i = 0; i < 6; i++){
+      this.fingers[i].update(smoothing);
+    }
+  }
+
+  increaseFramesSinceSeen(){
+    for (let i = 0; i < 6; i++){
+      this.fingers[i].increaseFramesSinceSeen();
+    }
+  }
+  drawLines(){
+
+    // 0 is the wrist, so skip it for now
+    for (let i = 1; i < 6; i++){
+      this.fingers[i].drawLine();
+    }
+  }
+}
+
+leftFingers = new fingerPoints();
+rightFingers = new fingerPoints();
+//--------------------------------------------------------------------------------
+
 
 let fingersSmoothedLeft = [];
 let fingersSmoothedRight = [];
-let sliders = [];
-let checkboxes =[];
-var f;
+
+
+
+
 
 function preload() {
   // Load the handpose model.
   handpose = ml5.handpose();
-  f = loadFont(
-    "https://cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/font/SourceCodePro-Bold.otf"
-  );
-}
-
-function pushEmptyPt(arry, idx) {
-  arry[idx] = [];
-  for (i = 0; i < 5; i++) {
-    let pt = {};
-    pt.x = 0;
-    pt.y = 0;
-    pt.z = -9999; // css gives me bad habits
-    pt.nFramesSinceSeen = 100;
-    arry[idx].push(pt);
-  }
 }
 
 function setup() {
-  createCanvas(640, 480, WEBGL);
-  textFont(f);
+  framerate(25);
+  createCanvas(640*2, 480*2, WEBGL);
   // Create the webcam video and hide it
   video = createCapture(VIDEO);
-  video.size(width, height);
+  video.size(640, 480);
   video.hide();
   gl = this._renderer.GL;
   gl.disable(gl.DEPTH_TEST);
-  gl.enable(gl.BLEND);
-  gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
   // start detecting hands from the webcam video
+  // what are options for detectStart?
+  // 1. video: the webcam video
+  // 2. a callback function: gotHands
+
+
+  // const options = {
+  //   flipHorizontal: false, // boolean value for if the video should be flipped, defaults to false
+  //   maxContinuousChecks: Infinity, // How many frames to go without running the bounding box detector. Defaults to infinity, but try a lower value if the detector is consistently producing bad predictions.
+  //   detectionConfidence: 0.8, // Threshold for discarding a prediction. Defaults to 0.8.
+  //   scoreThreshold: 0.75, // A threshold for removing multiple (likely duplicate) detections based on a "non-maximum suppression" algorithm. Defaults to 0.75
+  //   iouThreshold: 0.3, // A float representing the threshold for deciding whether boxes overlap too much in non-maximum suppression. Must be between [0, 1]. Defaults to 0.3.
+  //   }
+
+    
+
   handpose.detectStart(video, gotHands);
-
-  for (var j = 0; j < lineLen; j++) {
-    pushEmptyPt(fingersSmoothedLeft, j);
-    pushEmptyPt(fingersSmoothedRight, j);
-  }
-
-  // slider setup
-  let xs = 410;
-  oneSlider = createSlider(0, 100, defaulType[0]);
-  oneSlider.position(120 + xs, 20);
-  oneSlider.style("width", "100px");
-  twoSlider = createSlider(0, 100, defaulType[1]);
-  twoSlider.position(120 + xs, 50);
-  twoSlider.style("width", "100px");
-  threeSlider = createSlider(0, 100, defaulType[2]);
-  threeSlider.position(120 + xs, 80);
-  threeSlider.style("width", "100px");
-  fourSlider = createSlider(0, 100, defaulType[3]);
-  fourSlider.position(120 + xs, 110);
-  fourSlider.style("width", "100px");
-
-  checkbox = createCheckbox('', true);
-
-  checkbox.position(120 + xs, 140);
-  checkbox.style("width", "100px");
-  sliders = [oneSlider, twoSlider, threeSlider, fourSlider,checkbox];
-  checkboxes= [checkbox]
-}
-
-function updateData(handedness, arry) {
-  if (hands.length == 0) {
-    pushEmptyPt(arry, startIdx);
-  }
-
-  for (let i = 0; i < hands.length; i++) {
-    if (hands[i].handedness == handedness) {
-      let hand = hands[i];
-      let fingerIdx = 0;
-      for (let h = 0; h < hand.keypoints.length; h++) {
-        if (h == 8 || h == 4 || h == 12 || h == 16 || h == 20) {
-          let keypoint = hand.keypoints[h];
-          if (arry[0][fingerIdx].nFramesSinceSeen > 10) {
-            arry[startIdx][fingerIdx].x = keypoint.x;
-            arry[startIdx][fingerIdx].y = keypoint.y;
-            // onsole.log(arry[startIdx][fingerIdx].x);
-          } else {
-            if (arry[startIdx - 1]) {
-              arry[startIdx][fingerIdx].x =
-                easing * arry[startIdx - 1][fingerIdx].x +
-                (1 - easing) * keypoint.x;
-              arry[startIdx][fingerIdx].y =
-                easing * arry[startIdx - 1][fingerIdx].y +
-                (1 - easing) * keypoint.y;
-            } else {
-              arry[startIdx][fingerIdx].x =
-                easing * arry[arry.length - 1][fingerIdx].x +
-                (1 - easing) * keypoint.x;
-              arry[startIdx][fingerIdx].y =
-                easing * arry[arry.length - 1][fingerIdx].y +
-                (1 - easing) * keypoint.y;
-            }
-          }
-          arry[startIdx][fingerIdx].z = 10;
-          fingerIdx++;
-        }
-      }
-    for (j = 0; j < 5; j++) {
-      arry[0][j].nFramesSinceSeen = 0;
-    }
-    } else if (hands.length == 1) {
-      pushEmptyPt(arry, startIdx);
-    }
-  }
-}
-
-
-function updategui() {
-  push();
-  translate(50, -height / 2 - 10);
-  //textFont('Verdana');
-  for (var i = 0; i < sliders.length; i++) {
-    // fill(255,150);
-    fill("white");
   
-    text(features[i], 55, 35 + i*30);
-    // text(sliders[i].value() + '%', sliders[i].x + 20 + sliders[i].width, 35 + i*30);
-    // text(features[i][0], sliders[i].x + 55 + sliders[i].width, 35 + i*30);
-    // // highlight in black
-    // fill(255);
-    // if ( sliders[i].value() < 50) {
-    //   text((100-sliders[i].value()) + '%', 20, 35 + i*30);
-    //   //text(features[i][1], 55, 35 + i*30);
-    // } else {
-    //   text(sliders[i].value() + '%', sliders[i].x + 20 + sliders[i].width, 35 + i*30);
-    //   text(features[i][0], sliders[i].x + 55 + sliders[i].width, 35 + i*30);
-    // }
-  }
-  pop();
-}
+  console.log(handpose);
 
-function drawLines(arry) {
-  for (var l = 0; l < lineLen; l++) {
-    let normIndex = (l + startIdx) % lineLen;
-    for (let tips = 0; tips < arry[startIdx].length; tips++) {
-      var lx = arry[normIndex][tips].x;
-      var ly = arry[normIndex][tips].y;
-      var lz = arry[normIndex][tips].z;
-      var _lx = lx;
-      var _ly = ly;
-      var _lz = lz;
+  leftFingers.setup();
+  rightFingers.setup();
 
-      if (arry[normIndex + 1]) {
-        _lx = arry[normIndex + 1][tips].x;
-        _ly = arry[normIndex + 1][tips].y;
-        _lz = arry[normIndex + 1][tips].z;
-      } else {
-        end = 0;
-        _lx = arry[end][tips].x;
-        _ly = arry[end][tips].y;
-        _lz = arry[end][tips].z;
-      }
+  // load a system font
+ 
+  textScreen = createGraphics(400,400)
+  
 
-      if (_lx !== undefined && _ly !== undefined && _lz !== undefined) {
-        let di = dist(lx, ly, lz, _lx, _ly, _lz);
-
-        if (di < d) {
-          let zfade = constrain(1 - lz / 50, 0, 1);
-          stroke(255 * zfade, 255 * zfade, 255 * zfade);
-          line(lx, ly, lz, _lx, _ly, _lz);
-        }
-      }
-      // move z index back
-      arry[normIndex][tips].z -= ((sliders[2].value())/100.) * 30.;
-    }
-  }
 }
 
 
 function draw() {
-//  clear();
-  startIdx = startIdx + 1;
-  startIdx = startIdx % lineLen;
-
-  //update slider values
-  easing = 0.9*(sliders[0].value()/100.0)
-   //lineLen = (sliders[0].value()/100.0)* maxLineLen
-  background(0)
   
-  for (let i = 0; i < 5; i++) {
-    fingersSmoothedLeft[0][i].nFramesSinceSeen++;
-    fingersSmoothedRight[0][i].nFramesSinceSeen++;
+  // assume we have not seen the fingers 
+  leftFingers.increaseFramesSinceSeen();
+  rightFingers.increaseFramesSinceSeen();
+
+  
+  for (i = 0; i < hands.length; i++){
+    if (hands[i].handedness == "Left"){
+      for (j = 0; j < 6; j++){
+        leftFingers.updateTarget(j, hands[i].keypoints[handPointInices[j]].x-video.width/2, 
+        hands[i].keypoints[handPointInices[j]].y-video.height/2); 
+      }
+    }
+    if (hands[i].handedness == "Right"){
+      for (j = 0; j < 6; j++){
+        rightFingers.updateTarget(j, hands[i].keypoints[handPointInices[j]].x-video.width/2, 
+        hands[i].keypoints[handPointInices[j]].y-video.height/2);
+      }
+    }
   }
 
-  //orbitControl();
+  leftFingers.update(0.6);
+  rightFingers.update(0.6);
+
+  // print framerate to the canvas
+
+
+  //console.log(frameRate());
+
   push();
- 
+  clear();
+  background(0);
+  orbitControl();
+
+  scale(1.4, 1.4, 1.4);
+
   rotateY(70);
-  translate(200, 0, -200);
+  //translate(200, 0, -200);
   push();
-     if (checkboxes[0].checked()){
-    scale(-1,1,1)
-  }
-
   noStroke();
 
   fill(255, 0, 0, 50);
-   tint(255, ((sliders[1].value())/100.0) * 255.0)
-  texture(video);
-  plane(width, height);
+  texture(video );
+  plane(video.width, video.height);
+
+  // Draw all left fingers
+  // for (i = 0; i < 5; i++){
+  //   fill(255, 0, 0);
+  //   ellipse(leftFingers.fingers[i].x, leftFingers.fingers[i].y, 10, 10);
+  // }
+
+  stroke(255);
+  leftFingers.drawLines();
+  rightFingers.drawLines();
+
+
   pop(); // return to our original coordinate system
 
   // Draw the webcam video
+  //image(video, 0, 0, width, height);
   push();
-     if (checkboxes[0].checked()){
-    scale(-1,1,1)
-    console.log("FLIPS!")
-  }
   translate(-width / 2, -height / 2, 10);
-
-  updateData("Left", fingersSmoothedLeft);
-  updateData("Right", fingersSmoothedRight);
-
-  // // Draw all the tracked hand points'
-  // for (j = 0; j < 5; j++) {
-  //   circle(
-  //     fingersSmoothedLeft[startIdx][j].x,
-  //     fingersSmoothedLeft[startIdx][j].y,
-  //     20
-  //   );
-  //   circle(
-  //     fingersSmoothedRight[startIdx][j].x,
-  //     fingersSmoothedRight[startIdx][j].y,
-  //     20
-  //   );
-  // }
-  drawLines(fingersSmoothedRight);
-  drawLines(fingersSmoothedLeft);
+  
   pop();
   pop();
-  updategui();
+
+  // clear textScreen
+  textScreen.clear();
+
+  // make a string with the frame rate that doesn't have a ton of decimal places
+  let string = "" + frameRate();
+  let n = string.indexOf(".");
+  if (n != -1){
+    string = string.substring(0, n+2);
+  }
+
+  
+  // set font size for textScreen
+  textScreen.textSize(32);
+  textScreen.fill(255);
+  textScreen.text("" + string, 30, 30);
+  image(textScreen, 0 - width/2,0 - height/2) 
 }
 
 // Callback function for when handpose outputs data
 function gotHands(results) {
   // save the output to the hands variable
   hands = results;
+
+  //console.log(hands);
 }
